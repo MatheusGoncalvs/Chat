3f6260dcbffa8463590777396fe951416f1d2549
matheusgoncalvs <matheusgoncalvs@hotmail.com>
2019-09-19 07:54:04 -0300

    Initial commit

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Chat.Models;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(Message message) =>
+            await Clients.All.SendAsync("receiveMessage", message);
+    }
+}
